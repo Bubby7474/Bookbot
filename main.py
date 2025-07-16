3e9480a19f5c103ed44ec26eecdf63878ff61775
Bubby7474 <dschooley6476@gmail.com>
2025-07-15 21:11:51 -0700

    Started Bookbot

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-print("greetings boots")
+def get_book_text(filepath):
+    file_path = filepath
+    with open(file_path) as f:
+        file_contents = f.read()
+    return file_contents
+def main():
+    book_text = get_book_text("workspace/bootdotdev/bookbot/books/frankenstein.txt")
+    print(book_text)
+main()
