19cc74bb5969f0acbf7c27bc03f7b2da7d05b6f6
Bubby7474 <dschooley6476@gmail.com>
2025-07-16 09:41:18 -0700

    Adjusted Arrangement and Moved Word count to Stats

--- main.py
+++ main.py
@@ -1,18 +1,16 @@
+def main():
+    book_text = get_book_text("books/frankenstein.txt")
+    # Above relative path (workspace/bootdotdev/bookbot/books/frankenstein.txt) works for VSCode
+    # However, for it to work in WSL, it needs to be (books/frankenstein.txt)
+    word_count = get_word_count(book_text)
+    print(f"{word_count} words found in the document")
+
 def get_book_text(filepath):
-    file_path = filepath
-    with open(file_path) as f:
+    with open(filepath) as f:
         file_contents = f.read()
     return file_contents
 
-def get_word_count(text):
-    count = text.split()
-    return len(count)
+from stats import get_word_count
 
-def main():
-    book_text = get_book_text("workspace/bootdotdev/bookbot/books/frankenstein.txt")
-    # Above relative path (workspace/bootdotdev/bookbot/books/frankenstein.txt) works for VSCode
-    # However, for it to work in WSL, it needs to be (books/frankenstein.txt)
-    print(book_text)
-    word_count = get_word_count(book_text)
-    print(f"{word_count} words found in the document")
+
 main()
---
+++ stats.py
@@ -0,0 +1,6 @@
+
+def get_word_count(text):
+    count = text.split()
+    return len(count)
+
+
