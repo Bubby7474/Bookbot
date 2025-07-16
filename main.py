802b471a46f8b1113cae6646bcc59289fd1f78cd
Bubby7474 <dschooley6476@gmail.com>
2025-07-15 21:35:19 -0700

    Added word count

--- main.py
+++ main.py
@@ -3,7 +3,15 @@ def get_book_text(filepath):
     with open(file_path) as f:
         file_contents = f.read()
     return file_contents
+
+def get_word_count(text):
+    count = text.split()
+    return len(count)
+
 def main():
     book_text = get_book_text("workspace/bootdotdev/bookbot/books/frankenstein.txt")
-    print(book_text)
+    # Above relative path (workspace/bootdotdev/bookbot/books/frankenstein.txt) works for VSCode
+    # However, for it to work in WSL, it needs to be (books/frankenstein.txt)
+    word_count = get_word_count(book_text)
+    print(f"{word_count} words were found in this document")
 main()
