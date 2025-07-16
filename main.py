320cfa7b00d841123970bb774775837d35a76745
Bubby7474 <dschooley6476@gmail.com>
2025-07-16 10:09:25 -0700

    Added Character Count

--- main.py
+++ main.py
@@ -1,8 +1,9 @@
 def main():
-    book_text = get_book_text("books/frankenstein.txt")
+    book_text = get_book_text("workspace/bootdotdev/bookbot/books/frankenstein.txt")
     # Above relative path (workspace/bootdotdev/bookbot/books/frankenstein.txt) works for VSCode
     # However, for it to work in WSL, it needs to be (books/frankenstein.txt)
     word_count = get_word_count(book_text)
+    character_count = get_character_count(book_text)
     print(f"{word_count} words found in the document")
 
 def get_book_text(filepath):
@@ -11,6 +12,7 @@ def get_book_text(filepath):
     return file_contents
 
 from stats import get_word_count
+from stats import get_character_count
 
 
 main()
--- stats.py
+++ stats.py
@@ -1,6 +1,16 @@
-
 def get_word_count(text):
-    count = text.split()
-    return len(count)
+    word_count = text.split()
+    return len(word_count)
+
+def get_character_count(words):
+    lower_case = words.lower()
+    character_count = {}
+    for letter in lower_case:
+        if letter not in character_count:
+            character_count[letter] = 1
+        else:
+            character_count[letter] += 1
+    print(character_count)
+
 
 
