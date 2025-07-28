5576e2c5f90bd8e7507791607f143c386506b3eb
Bubby7474 <dschooley6476@gmail.com>
2025-07-28 15:29:59 -0700

    Finished Bookbot

--- main.py
+++ main.py
@@ -1,18 +1,33 @@
 def main():
-    book_text = get_book_text("workspace/bootdotdev/bookbot/books/frankenstein.txt")
-    # Above relative path (workspace/bootdotdev/bookbot/books/frankenstein.txt) works for VSCode
-    # However, for it to work in WSL, it needs to be (books/frankenstein.txt)
-    word_count = get_word_count(book_text)
-    character_count = get_character_count(book_text)
-    print(f"{word_count} words found in the document")
+    import sys
+    if len(sys.argv) != 2:
+         print ("Usage: python3 main.py <path_to_book>")
+         sys.exit(1)
+    else:
+         book_text = sys.argv[1]
+    with open(book_text) as f:
+        contents = f.read()
+    word_count = get_word_count(contents)
+    character_count = get_character_count(contents)
+    character_list = sorted_list(character_count)
+    print("============ BOOKBOT ============")
+    print("Analyzing book found at books/frankenstein.txt...")
+    print("----------- Word Count ----------")
+    print(f"Found {word_count} total words")
+    print("--------- Character Count -------")
+    for i in character_list:
+        char = i["char"]
+        count = i["num"]
+        if char.isalpha():
+            print(f"{char}: {count}")
 
-def get_book_text(filepath):
-    with open(filepath) as f:
-        file_contents = f.read()
-    return file_contents
 
 from stats import get_word_count
 from stats import get_character_count
+from stats import sorted_list
+
+
+
 
 
 main()
--- stats.py
+++ stats.py
@@ -2,15 +2,29 @@ def get_word_count(text):
     word_count = text.split()
     return len(word_count)
 
+character_count = {}
+
 def get_character_count(words):
     lower_case = words.lower()
-    character_count = {}
     for letter in lower_case:
         if letter not in character_count:
             character_count[letter] = 1
         else:
             character_count[letter] += 1
-    print(character_count)
+
+def sorted_list(characters):
+    unsorted = character_count.items()
+    sorted_tuples = (sorted(unsorted, key= lambda item: item[1], reverse=True))
+    sorted_list = []
+    for char, count in sorted_tuples:
+        sorted_list.append({"char": char, "num": count})
+    return (sorted_list)
 
 
-
+#Keep below because it kinda works, but not correctly
+#Can potentially be modified to work, but for now
+#Work on above
+#def get_character_list(characters):
+    #character_list = character_count.items()
+    #new_list = sorted(character_list, reverse=True, key=lambda pair: pair[1])
+    #return (new_list)
